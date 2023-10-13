884fa3931c4428f6866d216f1b368eee221ca2c6
karamazovan <fn20404@bristol.ac.uk>
2023-10-13 14:44:00 +0100

    week2 Q3 interpolateThreeElementValues but failed float

--- src/RedNoise.cpp
+++ src/RedNoise.cpp
@@ -2,6 +2,7 @@
 #include <DrawingWindow.h>
 #include <Utils.h>
 #include <fstream>
+#include <iostream>
 #include <vector>
 #include <glm/glm.hpp>
 
@@ -21,17 +22,24 @@ std::vector<float> interpolateSingleFloats (float from, float to, size_t numberO
     return result;
 }
 
-/*
-vector<glm::vec3> interpolateThreeElementValues(glm::vec3 from, glm::vec3 to, size_t numberOfVectors) {
+// (x, y, z)
+std::vector<glm::vec3> interpolateThreeElementValues(glm::vec3 from, glm::vec3 to, size_t numberOfVectors) {
     std::vector<glm::vec3> result;
+    if (numberOfVectors == 0) {
+        result.push_back(from);
+        return result;
+    }
+    glm::vec3 betweenValue = (to - from) / glm::vec3(static_cast<float>(numberOfVectors - 1));
+    for (size_t i = 0; i < numberOfVectors; i++) {
+        result.push_back(from + glm::vec3(i) * betweenValue);
+    }
     return result;
 }
-*/
+
 
 
 void draw(DrawingWindow &window) {
 	window.clearPixels();
-
 	for (size_t y = 0; y < window.height; y++) {
 		for (size_t x = 0; x < window.width; x++) {
 		    // Use the x-coordinate as the intensity for a left-to-right gradient
@@ -59,13 +67,26 @@ void handleEvent(SDL_Event event, DrawingWindow &window) {
 
 int main(int argc, char *argv[]) {
 	DrawingWindow window = DrawingWindow(WIDTH, HEIGHT, false);
+
+    // W2 - Task 2: Single Element Numerical Interpolation
 	std::vector<float> result;
     result = interpolateSingleFloats(2.2, 8.5, 7);
 	for (size_t i = 0; i < result.size(); i++) {
 	    std::cout << result[i] << " ";
 	}
     std::cout << std::endl;
-	SDL_Event event;
+
+    // W2 - Task 4: Three Element Numerical Interpolation
+	glm::vec3 from(1.0, 4.0, 9.2);
+    glm::vec3 to(4.0, 1.0, 9.8);
+    size_t numberOfVectors = 4;
+    std::vector<glm::vec3> resultTEV = interpolateThreeElementValues(from, to, numberOfVectors);
+    for (size_t i = 0; i < resultTEV.size(); i++) {
+        std::cout << "(" << resultTEV[i].x << ", " << resultTEV[i].y << ", " << resultTEV[i].z << ")" << std::endl;
+    }
+
+    SDL_Event event;
+
 	while (true) {
 		// We MUST poll for events - otherwise the window will freeze !
 		if (window.pollForInputEvents(event)) handleEvent(event, window);
