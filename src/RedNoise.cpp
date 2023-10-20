ec2f850ccb6385729055bf0f350c91eba1fe0960
karamazovan <fn20404@bristol.ac.uk>
2023-10-20 01:57:20 +0100

    git config core.autocrlf

--- src/RedNoise.cpp
+++ src/RedNoise.cpp
@@ -14,6 +14,8 @@
 #define WIDTH 320
 #define HEIGHT 240
 
+using namespace glm;
+
 std::vector<float> interpolateSingleFloats (float from, float to, size_t numberOfValues) {
     std::vector<float> result;
     if (numberOfValues == 0 ) {
@@ -34,7 +36,9 @@ std::vector<glm::vec3> interpolateThreeElementValues(glm::vec3 from, glm::vec3 t
         result.push_back(from);
         return result;
     }
-    glm::vec3 betweenValue = (to - from) / glm::vec3(numberOfVectors - 1);
+    glm::vec3 divisor(numberOfVectors - 1, numberOfVectors - 1, numberOfVectors - 1);
+    glm::vec3 betweenValue = (to - from) / divisor;
+    // glm::vec3 betweenValue = (to - from) / glm::vec3(numberOfVectors - 1);
     for (size_t i = 0; i < numberOfVectors; i++) {
         result.push_back(from + betweenValue * glm::vec3(i));
     }
@@ -47,6 +51,16 @@ uint32_t colourPalette(Colour colour) {
 
 Colour randomColour() {
     return Colour(rand() % 256, rand() % 256, rand() % 256);
+}
+
+float interpolation(float x, float x1, float x2, float y1, float y2) {
+    // shouldn't be equal to avoid division by zero
+    if (x1 == x2) {
+        return y1;
+    }
+    float slope = (y2 - y1) / (x2 - x1);
+    float y = y1 + (x - x1) * slope;
+    return y;
 }
 
 CanvasTriangle randomLines() {
@@ -114,14 +128,50 @@ void triangleRasteriser(CanvasTriangle triangle, Colour colour, DrawingWindow &w
         float xEnd = x0 + (y - y0) * slope02;
         drawLine(CanvasPoint(xStart, y), CanvasPoint(xEnd, y), colour, window);
     }
-
     // draw white line
     Colour whiteLine = Colour(255, 255, 255);
     drawTriangle(triangle, whiteLine, window);
 }
 
-void draw(DrawingWindow &window) {
+void textureMapper(CanvasTriangle triangle, TextureMap &textureMap, DrawingWindow &window) {
+    // Sort the vertices by y coordinates
+    if (triangle.v0().y > triangle.v1().y) std::swap(triangle.v0(), triangle.v1());
+    if (triangle.v0().y > triangle.v2().y) std::swap(triangle.v0(), triangle.v2());
+    if (triangle.v1().y > triangle.v2().y) std::swap(triangle.v1(), triangle.v2());
 
+    for (size_t y = triangle.v0().y; y < triangle.v2().y; y++) {
+        float xStart = interpolation(y, triangle.v0().y, triangle.v1().y, triangle.v0().x, triangle.v1().x);
+        float xEnd = interpolation(y, triangle.v0().y, triangle.v2().y, triangle.v0().x, triangle.v2().x);
+
+        TexturePoint textureStart = TexturePoint(interpolation(y,triangle.v0().y, triangle.v1().y, triangle.v0().texturePoint.x, triangle.v1().texturePoint.x),
+                                                 interpolation(y,triangle.v0().y, triangle.v1().y, triangle.v0().texturePoint.y, triangle.v1().texturePoint.y));
+        TexturePoint textureEnd = TexturePoint(interpolation(y,triangle.v0().y, triangle.v2().y, triangle.v0().texturePoint.x, triangle.v2().texturePoint.x),
+                                                 interpolation(y,triangle.v0().y, triangle.v2().y, triangle.v0().texturePoint.y, triangle.v2().texturePoint.y));
+        float distance = xEnd - xStart;
+        std::vector<float> interpolationX = interpolateSingleFloats(xStart, xEnd, distance);
+        std::vector<glm::vec3> interpolationTexture = interpolateThreeElementValues(glm::vec3(textureStart.x, textureStart.y, 0), glm::vec3(textureEnd.x, textureEnd.y, 0), distance);
+
+        for (size_t i = 0; i < interpolationX.size(); i++) {
+            int textureX = std::floor(interpolationTexture[i].x);
+            int textureY = std::floor(interpolationTexture[i].y);
+            int digit = textureY * textureMap.width + textureX;
+
+            if (digit >= 0 && digit < textureMap.pixels.size()) {
+                uint32_t textureColour = textureMap.pixels[digit];
+                window.setPixelColour(std::round(interpolationX[i]), y, textureColour);
+            }
+        }
+    }
+}
+
+void draw(DrawingWindow &window) {
+    window.clearPixels();
+    TextureMap textureMap("./src/texture.ppm");
+    CanvasTriangle triangle(CanvasPoint(160, 10), CanvasPoint(300, 230), CanvasPoint(10, 150));
+    triangle.v0().texturePoint = TexturePoint(195, 5);
+    triangle.v1().texturePoint = TexturePoint(395, 380);
+    triangle.v2().texturePoint = TexturePoint(65, 330);
+    textureMapper(triangle, textureMap, window);
 }
 
 void handleEvent(SDL_Event event, DrawingWindow &window) {
@@ -156,7 +206,7 @@ int main(int argc, char *argv[]) {
     std::cout << std::endl;
 
     // W2 - Task 4: Three Element Numerical Interpolation
-	glm::vec3 from(1.0, 4.0, 9.2);
+    glm::vec3 from(1.0, 4.0, 9.2);
     glm::vec3 to(4.0, 1.0, 9.8);
     for (glm::vec3 vec : interpolateThreeElementValues(from, to, 4)) {
         std::cout << "(" << vec.x << ", " << vec.y << ", " << vec.z << ")" << std::endl;
@@ -164,7 +214,7 @@ int main(int argc, char *argv[]) {
 
     SDL_Event event;
 
-	while (true) {
+    while (true) {
 		// We MUST poll for events - otherwise the window will freeze !
 		if (window.pollForInputEvents(event)) handleEvent(event, window);
 		draw(window);
